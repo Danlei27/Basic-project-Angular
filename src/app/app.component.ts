d20ef69cb79e402fd0b7e9fde672f39955a10374
danlei27 <danleisantos@hotmail.com>
2020-06-26 00:14:27 -0300

    Navigation lists (exemple)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'basic-project';
+  typesOfShoes: string[] = ['Boots', 'Clogs', 'Loafers', 'Moccasins', 'Sneakers'];
 }
