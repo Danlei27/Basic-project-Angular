bc3d84f968e23842da1749d896f1f61032ca2bb3
danlei27 <danleisantos@hotmail.com>
2020-06-29 21:46:00 -0300

    generate components and toobar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material-module'
+import { MaterialModule } from './material-module';
+import { UsuarioComponent } from './usuario/usuario.component';
+import { ListaProdutosComponent } from './lista-produtos/lista-produtos.component';
+import { LogoutComponent } from './logout/logout.component';
+import { HomeComponent } from './home/home.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsuarioComponent,
+    ListaProdutosComponent,
+    LogoutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
