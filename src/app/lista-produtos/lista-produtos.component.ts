de0046900fe46403da7b12b593a3b1cb02d6db2d
danlei27 <danleisantos@hotmail.com>
2020-07-06 21:12:59 -0300

    criando features da tabela, Data table Pagination, Filtering, Sorting.

--- src/app/lista-produtos/lista-produtos.component.ts
+++ src/app/lista-produtos/lista-produtos.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ListaService } from './lista.service';
 import { Produtos } from './produtos';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-lista-produtos',
@@ -8,18 +11,26 @@ import { Produtos } from './produtos';
   styleUrls: ['./lista-produtos.component.css']
 })
 export class ListaProdutosComponent implements OnInit {
-
-  displayedColumns: string[] = ['position', 'name', 'weight', 'button'];
-  dataSource: Produtos[];
+  
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
+  
+  produtos: Produtos[];
+  displayedColumns: string[] = ['id', 'nome', 'quantidades', 'button'];
   
   title = 'Almoxarifado';
-  produtos: Produtos[];
+  dataSource =  new MatTableDataSource(this.produtos)
 
   constructor(private service: ListaService) { }
 
   ngOnInit(): void {
-    this.service.list().subscribe(dados => this.dataSource = dados);
+    this.service.list().subscribe(dados => this.dataSource.data =  dados);
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
     
   }
 
+  applyFilter(filterValue: string){
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
